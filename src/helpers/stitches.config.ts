17df88d2db09f612f9b6543c7eb5c862d6611188
Salv-Dev <salv.dev.br@gmail.com>
2023-01-16 18:24:08 -0300

    adiciona o arquivo de configuração do stitches

---
+++ src/helpers/stitches.config.ts
@@ -0,0 +1,24 @@
+import { createStitches } from "@stitches/react";
+
+export const { 
+  styled, 
+  getCssText, 
+  createTheme, 
+  globalCss 
+} = createStitches({ 
+  theme: {
+    colors: {
+      text: "black",
+      background: "white",
+    },
+    sizes: {
+        1: '14px',
+        2: '16px',
+        3: '18px'
+    },
+    space: {
+        1: '10px',
+        2: '20px'
+    }
+  }
+ });
